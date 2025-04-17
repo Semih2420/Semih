17cf4bbff125516613068ddaca10a3f5899f9f94
Semih2420 <120945390+Semih2420@users.noreply.github.com>
2025-04-17 22:28:44 +0200

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 
 public class Hello {
-	public static void main(String[] args) {
-		System.out.println("Ich werde \"Informagier\"!");
+	public static void main (String [] args) {
+		
+		System.out.println("Guten Tag");
 	}
+
 }
